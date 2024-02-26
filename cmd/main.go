4cff254a73b6df19ff657565a9208a2e4d7e70f4
Saurabh Kumar <saurabh7517@gmail.com>
2024-02-26 19:31:41 +0000

    ping message from client

--- cmd/main.go
+++ cmd/main.go
@@ -2,13 +2,16 @@ package main
 
 import (
 	"fmt"
+	"irc_client/pkg"
 	"log"
 	"net"
+
+	"google.golang.org/protobuf/proto"
 )
 
-const SERVER_HOST = "192.168.3.6"
-const SERVER_PORT = "9988"
-const SERVER_TYPE = "tcp"
+const SERVER_HOST = "127.0.0.1"
+const SERVER_PORT = "8080"
+const SERVER_TYPE = "tcp4"
 
 func main() {
 	var address string = fmt.Sprintf("%v:%v", SERVER_HOST, SERVER_PORT)
@@ -19,6 +22,34 @@ func main() {
 	defer connnection.Close()
 	// initialize the size of buffer 512 bytes
 	// send
-	var msgBytes []byte = []byte("Hello from client")
+	// var msgBytes []byte = []byte("Hello from client")
+	var pingMessage *pkg.Message = &pkg.Message{Command: pkg.Command_Ping}
+	var msgBytes []byte = encodeMessage(pingMessage)
+
 	connnection.Write(msgBytes)
+
+	var incomingBytes []byte = make([]byte, 512)
+
+	incomingByteLen, err := connnection.Read(incomingBytes)
+	if err != nil {
+		log.Println("Error reading from the connection buffer")
+	}
+ 	log.Printf("Read %d from connection buffer", incomingByteLen)
+	var incomMsg *pkg.Message = decodeMessage(incomingBytes)
+	log.Println("Server :: ", incomMsg.Command)
+
+}
+
+func encodeMessage(message *pkg.Message) []byte {
+	msgBytes, err := proto.Marshal(message)
+	if err != nil {
+		log.Println("Error in marshaling data")
+	}
+	return msgBytes
+}
+
+func decodeMessage(message []byte) *pkg.Message {
+	var incomMsg *pkg.Message = &pkg.Message{}
+	proto.Unmarshal(message, incomMsg)
+	return incomMsg
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module irc_client
 
 go 1.21.6
+
+require google.golang.org/protobuf v1.32.0 // indirect
