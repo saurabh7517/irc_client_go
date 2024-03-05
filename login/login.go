32eb9a075592b649c9c95d4d309521882328e4da
saurabh7517 <saurabh7517@gmail.com>
2024-03-05 11:54:20 +0000

    test

--- login/login.go
+++ login/login.go
@@ -7,7 +7,7 @@ import (
 )
 
 func ProcessLogin(reader *bufio.Reader) []byte {
-	inputUser, inputPassword := pkg.ReadUser(reader)
+	inputUser, inputPassword := pkg.ReadUser(reader) //TODO
 	user, hostAddress := pkg.CreateUserAndHostMessage(inputUser, inputPassword)
 	var userLoginData *obj.Message = &obj.Message{Command: obj.Command_Log, User: user, HostAddress: hostAddress}
 	return pkg.EncodeMessage(userLoginData)
