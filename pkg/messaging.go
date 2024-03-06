73461fcef2368282581afc031de61a99dd7d6cb9
saurabh7517 <saurabh7517@gmail.com>
2024-03-06 16:33:29 +0000

    started with getAll Users logic

--- pkg/messaging.go
+++ pkg/messaging.go
@@ -1,19 +1,52 @@
 package pkg
 
 import (
+	"bufio"
+	"fmt"
 	obj "irc_client/objects"
+	"log"
 	"net"
+	"os"
 )
 
 func ProcessMessaging(conn net.Conn, token string) {
 	//list all active users
 	//create a go routine which will listen for new added active users
-	getAllUsers(conn, token)
+	var userList []string = getAllUsers(conn, token)
+	if len(userList) > 0 {
+		showActiveUsers(userList)
+		fmt.Println("Enter the name of the user, you want to chat with...")
+		reader := bufio.NewReader(os.Stdin)
+		userInput, err := reader.ReadString('\n')
+		if err != nil {
+			log.Println("Error reading from console")
+		}
+		var found bool = false
+		if len(userInput) > 0 {
+			for _, user := range userList {
+				if user == userInput {
+					found = true
+					break
+				}
+			}
+		}
+		if found {
+			// send a message to the user on connection
+			fmt.Print("Your message : >") //TODO own username
+			message, _ := reader.ReadString('\n')
+
+		} else {
+			log.Println("User entered is not among active user list")
+		}
+
+	}
 
 }
 
-func showActiveUsers() {
-
+func showActiveUsers(userList []string) {
+	for _, user := range userList {
+		fmt.Println(user)
+	}
 }
 
 func showActiveChannels() {
