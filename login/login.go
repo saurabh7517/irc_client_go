8fddf1fc3ea49122f71c9a283a32d12ebb3d6937
Saurabh Kumar <saurabh7517@gmail.com>
2024-03-07 11:59:09 +0000

    storing logged in user to cache

--- login/login.go
+++ login/login.go
@@ -2,13 +2,43 @@ package login
 
 import (
 	"bufio"
+	"fmt"
+
 	obj "irc_client/objects"
 	"irc_client/pkg"
+
+	"net"
 )
 
-func ProcessLogin(reader *bufio.Reader) []byte {
-	inputUser, inputPassword := pkg.ReadUser(reader) //TODO
-	user, hostAddress := pkg.CreateUserAndHostMessage(inputUser, inputPassword)
+type UserSession struct {
+	Name        string
+	token       string
+	HostAddress string
+}
+
+func storeUserSession(username string, token string, hostAddress string) {
+	LoginCache[username] = UserSession{Name: username, token: token, HostAddress: hostAddress}
+}
+
+var LoginCache map[string]UserSession = make(map[string]UserSession)
+
+func ProcessLogin(reader *bufio.Reader, connection net.Conn) {
+	inputUsername, inputPassword := pkg.ReadUser(reader) //TODO
+	var response *obj.Response = processUserInput(inputUsername, inputPassword, connection)
+	if response.Msg == "SUCCESS" && response.Token != "" {
+		fmt.Println("User logger in")
+		storeUserSession(inputUsername, response.Token, connection.LocalAddr().String())
+		ProcessMessaging(connection, response.Token)
+	} else if response.Msg == "FAILED" {
+		fmt.Println("Login Failed, check username and password")
+	} else {
+		fmt.Println("Error on server, contact your admin !")
+	}
+}
+
+func processUserInput(inputUsername string, inputPassword string, connection net.Conn) *obj.Response {
+	user, hostAddress := pkg.CreateUserAndHostMessage(inputUsername, inputPassword)
 	var userLoginData *obj.Message = &obj.Message{Command: obj.Command_Log, User: user, HostAddress: hostAddress}
-	return pkg.EncodeMessage(userLoginData)
+	var logMsgBytes []byte = pkg.EncodeMessage(userLoginData)
+	return pkg.SendRQGetRS(connection, logMsgBytes)
 }
